13e28329f7ce802424760b597d5cf11bc982a0ea
seonpilKim <ksp970306@gmail.com>
2024-01-14 19:45:54 +0900

    #10 feat: implement AES encrypt/decrypt.
    - support AES cipher mode: GCM/NoPadding, CTR/NoPadding

---
+++ src/main/java/com/titi/crypto/exception/TiTiCryptoException.java
@@ -0,0 +1,13 @@
+package com.titi.crypto.exception;
+
+public class TiTiCryptoException extends RuntimeException {
+
+	public TiTiCryptoException(String message) {
+		super(message);
+	}
+
+	public TiTiCryptoException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
