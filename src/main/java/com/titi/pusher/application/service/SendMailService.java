0920e048a39882b1d036f23cd4c957fe080327c3
seonpilKim <ksp970306@gmail.com>
2024-01-17 23:08:54 +0900

    #4 feat: implement SendMailService.
    - add out ports: SendMailPort, CreateNotificationPort, UpdateNotificationPort.
    - add SimpleMailMessage Domain class.

---
+++ src/main/java/com/titi/pusher/application/service/SendMailService.java
@@ -0,0 +1,69 @@
+package com.titi.pusher.application.service;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import com.titi.pusher.application.port.in.email.SendMailUseCase;
+import com.titi.pusher.application.port.out.persistence.notification.CreateNotificationPort;
+import com.titi.pusher.application.port.out.persistence.notification.UpdateNotificationPort;
+import com.titi.pusher.application.port.out.smtp.SendMailPort;
+import com.titi.pusher.domain.email.SimpleMailMessage;
+import com.titi.pusher.domain.notification.Notification;
+import com.titi.pusher.domain.notification.ServiceInfo;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+class SendMailService implements SendMailUseCase {
+
+	private final SendMailPort sendMailPort;
+	private final CreateNotificationPort createNotificationPort;
+	private final UpdateNotificationPort updateNotificationPort;
+
+	@Override
+	@Transactional
+	public Notification invoke(Command command) {
+		return switch (command) {
+			case Command.ToSimpleMessage message -> {
+				final Notification notification = this.createNotification(message);
+				final boolean isSuccessful = this.sendNotification(message);
+				log.info("[SendMailUseCase] {} to send mail. notificationId: {}", isSuccessful ? "Success" : "Fail", notification.notificationId());
+				yield this.finishNotification(notification, isSuccessful);
+			}
+		};
+	}
+
+	private Notification createNotification(Command.ToSimpleMessage message) {
+		Notification notification = Notification.ofEmail(
+			message.notificationCategory(),
+			message.to(),
+			ServiceInfo.builder()
+				.name(message.serviceName())
+				.requestId(message.serviceRequestId())
+				.build()
+		);
+		this.createNotificationPort.create(notification);
+		return notification;
+	}
+
+	private boolean sendNotification(Command.ToSimpleMessage message) {
+		return this.sendMailPort.sendSimpleMessage(
+			SimpleMailMessage.builder()
+				.from(message.from())
+				.to(message.to())
+				.subject(message.subject())
+				.text(message.text())
+				.build()
+		);
+	}
+
+	private Notification finishNotification(Notification notification, boolean isSuccessful) {
+		notification = isSuccessful ? notification.complete() : notification.fail();
+		this.updateNotificationPort.update(notification);
+		return notification;
+	}
+
+}
