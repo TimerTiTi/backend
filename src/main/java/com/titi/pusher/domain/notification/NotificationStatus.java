f800fbb9e16a713a7fd8aeede6a115eacfa4ce6b
seonpilKim <ksp970306@gmail.com>
2024-01-17 22:46:17 +0900

    #4 feat: specify SendMailUseCase and add Notification domain class.

---
+++ src/main/java/com/titi/pusher/domain/notification/NotificationStatus.java
@@ -0,0 +1,12 @@
+package com.titi.pusher.domain.notification;
+
+public enum NotificationStatus {
+	/**
+	 * Notification delivery completed successfully.
+	 */
+	COMPLETED,
+	/**
+	 * Notification delivery failed.
+	 */
+	FAILED
+}
---
+++ src/main/java/com/titi/pusher/domain/notification/TargetInfo.java
@@ -0,0 +1,32 @@
+package com.titi.pusher.domain.notification;
+
+public sealed interface TargetInfo {
+
+	TargetType getType();
+
+	String getTargetValue();
+
+	enum TargetType {
+		/**
+		 * Target value would be encrypted and stored as the recipient's email address in the database.
+		 */
+		EMAIL
+	}
+
+	record EmailTargetInfo(
+		String email
+	) implements TargetInfo {
+
+		@Override
+		public TargetType getType() {
+			return TargetType.EMAIL;
+		}
+
+		@Override
+		public String getTargetValue() {
+			return this.email;
+		}
+
+	}
+
+}
