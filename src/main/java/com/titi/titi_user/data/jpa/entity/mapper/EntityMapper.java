d0a1e704975841106706e06fc08b124dd33dc507
seonpilKim <ksp970306@gmail.com>
2024-03-10 04:23:36 +0900

    fix #45 : add @Mapper annotation.

--- src/main/java/com/titi/titi_user/data/jpa/entity/mapper/EntityMapper.java
+++ src/main/java/com/titi/titi_user/data/jpa/entity/mapper/EntityMapper.java
@@ -1,10 +1,12 @@
 package com.titi.titi_user.data.jpa.entity.mapper;
 
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 import com.titi.titi_user.data.jpa.entity.MemberEntity;
 import com.titi.titi_user.domain.member.Member;
 
+@Mapper
 public interface EntityMapper {
 
 	EntityMapper INSTANCE = Mappers.getMapper(EntityMapper.class);
